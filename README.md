9681b3464e051883f5eab4898dd7bb137eb44105
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2023-01-26 18:28:41 +0900

    use vec

---
+++ README.md
@@ -0,0 +1,4 @@
+### feature may not be stable channel
+- nightlyにしたがだめ
+- cargo cleanで治ラズ
+- rustup にtargetを追加し、nightlyに変更。記載通り
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,38 @@
+#![feature(alloc_error_handler)]
 #![no_std]
 #![no_main]
 
+extern crate alloc;
+extern crate panic_halt;
+
+use alloc::alloc::{GlobalAlloc, Layout};
+use alloc::vec::Vec;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{debug, hprintln};
-use stm32f4xx_hal::{delay::Delay, prelude::*, stm32};
+
+struct MyAllocator;
+
+unsafe impl GlobalAlloc for MyAllocator {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        0x2000_0100 as *mut u8
+    }
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
+}
+
+#[global_allocator]
+static A: MyAllocator = MyAllocator;
+
+#[alloc_error_handler]
+fn on_oom(_layout: Layout) -> ! {
+    loop {}
+}
 
 #[entry]
 fn main() -> ! {
-    if let (Some(dp), Some(cp)) = (stm32::Peripherals::take(), stm32::CorePeripherals::take()) {
-        let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
-
-        let mut delay = Delay::new(cp.SYST, clocks);
-
-        let gpiod = dp.GPIOD.split();
-        let mut led = gpiod.pd15.into_push_pull_output();
-
-        for _ in 0..5 {
-            led.set_high().unwrap();
-            delay.delay_ms(100_u32);
-            led.set_low().unwrap();
-            delay.delay_ms(100_u32);
-        }
-    }
+    let mut x = Vec::new();
+    x.push(123);
+    x.push(456);
+    let _ = hprintln!("{:?}", x);
+    debug::exit(debug::EXIT_SUCCESS);
+    loop {}
 }
